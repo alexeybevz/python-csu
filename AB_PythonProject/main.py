7c6317d59e5968928fa27d89f8e9fc8d6ad84733
alexeybevz <bevz.alexei@yandex.ru>
2020-11-06 20:44:34 +0500

    refactoring

--- AB_PythonProject/main.py
+++ AB_PythonProject/main.py
@@ -1,38 +1,53 @@
-def main():
-    whse = Whse()
-
-    while True:
-        print("Введение название:")
-        name = input()
-
-        print("Введение количество:")
-        qty = input()
-
-        good = Good(name, qty)
-
-        whse.addGood(good)
-
-        print("Вывести все:")
-        resultDialog = input()
-
-        if resultDialog == "да":
-            print("Информация о складе:")
-            for good in whse.getGoods():
-                print(good.name + " - " + str(good.qty))
-
 class Good:
     def __init__(self, name, qty):
         self.name = name
         self.qty = qty
 
+
 class Whse:
-    goods = []
+    def __init__(self):
+        self.goods = {}
 
     def addGood(self, good):
-        if good is not None:
-            self.goods.append(good)
+        self.goods[good.name] = good.qty
+        # self.goods.append(good)
 
     def getGoods(self):
         return self.goods
 
-main()
+    def delGoods(self, key):
+        self.goods.pop(key, None)
+
+
+def add(whse):
+    print("Введение название:")
+    name = input()
+    print("Введение количество:")
+    qty = input()
+    good = Good(name, qty)
+
+    whse.addGood(good)
+
+
+def report(whse):
+    print("Информация о складе:")
+    info = whse.getGoods()
+    for good in whse.getGoods().keys():
+        print(f'{good} - {info[good]}')
+    print('\n')
+
+
+whse = Whse()
+while True:
+    print('1-добавить\n2-Вывести все\n3-удалить\n4-выйти\n')
+    name = int(input())
+    if name == 1:
+        add(whse)
+    elif name == 2:
+        report(whse)
+    elif name == 3:
+        report(whse)
+        key = input('введите ключ\n')
+        whse.delGoods(key)
+    else:
+        break
